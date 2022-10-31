82096f751cea17a3a8668bd7cf89dc5e9ac53922
RodrigoRG1921 <rodrigo1921r@gmail.com>
2022-10-31 15:53:47 -0600

    changed conclusion prompt and model

--- pages/api/spanish/conclusion.ts
+++ pages/api/spanish/conclusion.ts
@@ -7,26 +7,29 @@ const openai = new OpenAIApi(configuration);
 /* eslint import/no-anonymous-default-export: [2, {"allowAnonymousFunction": true}] */
 export default async function (req: any, res: any) {
   const completion = await openai.createCompletion({
-    model: 'text-davinci-002',
-    prompt: generatePrompt(req.subject, req.objective, req.steps),
-    temperature: 0.6,
+    model: 'text-curie-001',
+    max_tokens: 400,
+    prompt: generatePrompt(
+      req.body.subject,
+      req.body.objective,
+      req.body.steps
+    ),
+    temperature: 0.7,
   });
   res.status(200).json({ result: completion.data.choices![0].text });
 }
 
-function generatePrompt(tema: string, objetivo: string, pasos: string) {
-  return `Crea una conclusión de un ensayo del siguiente tema.
+function generatePrompt(subject: string, objective: string, steps: string) {
+  console.log(subject);
+  return `Crea una conclusión de una práctica del siguiente tema.
+
 tema: Flex Sim
 objetivo: trazar ruta y almacenar objetos en racks en nuestra simulacion
 pasos: simulamos un proceso de calidad de tres productos diferentes
-conclusión: Flex Sim nos permite trazar rutas y almacenar objetos en racks en nuestras simulaciones.
-En esta práctica simulamos un proceso de calidad de tres productos diferentes al igual que la practica anterior, con la diferencia de que al final, un transportador lleva cada producto a su rack correspondiente.
-Utilizando los nodos de red podemos trazar cualquier tipo de ruta para que nuestro transportador la siga y pueda llegar a su rack correspondiente. Flex sim nos permite trazar curvas y líneas rectas para que el transportador pueda seguir.
-Es por esto que Flex Sim es un programa bastante útil, permitiéndonos trazar rutas similares al mundo real.
-tema:
-tema: ${tema}
-objetivo: ${objetivo}
-pasos: ${pasos}
+conclusión: Flex Sim nos permite trazar rutas y almacenar objetos en racks en nuestras simulaciones. En esta práctica simulamos un proceso de calidad de tres productos diferentes al igual que la practica anterior, con la diferencia de que al final, un transportador lleva cada producto a su rack correspondiente. Es por esto que Flex Sim es un programa bastante útil, permitiéndonos trazar rutas similares al mundo real.
+tema: ${subject}
+objetivo: ${objective}
+pasos: ${steps}
 conclusión:
 `;
 }
