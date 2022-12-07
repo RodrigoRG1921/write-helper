1d9e18aa879f9c7e84e6f81732e19e9524af3e3b
RodrigoRG1921 <rodrigo1921r@gmail.com>
2022-12-06 18:57:21 -0600

    Initial next auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,10 @@ import type { NextPage } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
 import ai from '../public/ai.png';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 const Home: NextPage = () => {
+  const { data: session } = useSession();
   return (
     <div className='flex mx-10 mt-10'>
       <div className='flex flex-1 h-full flex-col p-5'>
@@ -13,14 +15,13 @@ const Home: NextPage = () => {
           inspiration, this website can help. Using artificial intelligence, we
           can help you write better, faster, and more efficiently.
         </p>
-        <Link href={'/create'}>
-          <button
-            type='button'
-            className='w-1/2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-          >
-            Get started
-          </button>
-        </Link>
+        <button
+          type='button'
+          onClick={() => signIn()}
+          className='w-1/2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+        >
+          {session ? 'Write something!' : 'Get Started'}
+        </button>
       </div>
       <div className='md:flex flex-1 h-full flex-col p-5 hidden'>
         <div className='mx-auto'>
